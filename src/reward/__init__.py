cccb45b6d5b62670e068e5dc13090e44558892dc
dzungpham <23127003@student.hcmus.edu.vn>
2026-06-22 17:23:49 +0700

    remove old files

--- src/reward/__init__.py
+++ src/reward/__init__.py
@@ -1,7 +1,5 @@
 from .base_reward import extract_call, schema_valid, func_selection_ok, args_accuracy
 from .rc_grpo_reward import rc_grpo_reward
-from .awpo_reward import awpo_reward
-from .gvpo_reward import gvpo_reward_func
 
 __all__ = [
     "extract_call",
